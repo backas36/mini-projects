d6cd3c8f430f26942994acb28e056cef291e1afe
backas36 <backas36@gmail.com>
2023-01-17 10:30:44 +0800

    day4

---
+++ day4-hidden-search-widget/script.js
@@ -0,0 +1,10 @@
+// https://github.com/bradtraversy/50projects50days
+
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
